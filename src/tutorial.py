29332e045a46ec9a2dc5fb12d5fc5d5893fed96a
Zuna0hu <mythhuza@gmail.com>
2025-01-11 18:16:05 -0500

    Initialize the main function of tutorial.py

--- src/tutorial.py
+++ src/tutorial.py
@@ -17,4 +17,36 @@ pygame.display.set_caption("My First Game")
 # set the background color
 BG_COLOR = (255, 255, 255) # white
 
+# set the height and width 
+WIDTH, HEIGHT = 1000, 800
+
+# frames per sec
+FPS = 60
+
+# play velocity
+PLAYER_VEL = 5
+
+# display the window
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    # set the clock
+    clock = pygame.time.Clock()
+
+    # event loop
+    run = True
+    while (run):
+        clock.tick(FPS) # to ensure the game loop does not run faster than 60 frames per sec
+
+        # to deal with different events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    
+
+# if the file is run directly
+if __name__ == "__main__":
+    main(window)
 
