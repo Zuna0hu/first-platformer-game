a6c19b403b13c35e73e8f7a01f738450f5aa9818
Zuna0hu <mythhuza@gmail.com>
2025-01-11 17:55:31 -0500

    Initialize tutorial.py
    This is the supporting file that we can refer to when implementing
    main.py.

---
+++ src/tutorial.py
@@ -0,0 +1,20 @@
+# import libraries
+import os
+import random
+import math
+import pygame
+
+# for read files quicker
+from os import listdir
+from os.path import isfile, join
+
+# initialize the game
+pygame.init()
+
+# set the title (caption) of the window
+pygame.display.set_caption("My First Game")
+
+# set the background color
+BG_COLOR = (255, 255, 255) # white
+
+
